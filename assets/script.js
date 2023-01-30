741e1425b3ec11fb02baa79cb250cd40411069ab
m1taylor <m1taylor@ryerson.ca>
2023-01-29 20:14:25 -0500

    fixed variables and url function

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,36 @@
-function initPage() {
-    const cityEl = document.getElementById ("city-input");
-    const searchEl = document.getElementById ("search-button");
+    // my api key
+    var weatherApiKey =  "2b607860bec061627a4d53da7828381b";
+    // other variables
+    var searchBtn = document.getElementById("search-button");
+    var searchInput = document.getElementById("city-input");
+    var historyContainer = document.getElementById("search-history");
+    var cityName = document.getElementById("city-name");
+    var temp = document.getElementById("temperature");
+    var humidity = document.getElementById("humidity");
+    var wind = document.getElementById("wind");
+    var fiveDay = document.getElementById("five-day");
+    var searchHistoryArr = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    var currentIconEl = document.getElementById ("weather-icon");
     const clearEl = document.getElementById ("clear-history");
-    const viewEl = document.getElementById ("view-history");
-    const nameEl = document.getElementById ("city-name");
-    const currentIconEl = document.getElementById ("weatherIcon");
-    const currentTempEl = document.getElementById ("temperature");
-    const currentHumEl = document.getElementById ("humidity");
-    const currentWindEl = document.getElementById ("windspeed");
-    const historyEl = document.getElementById("search-history")
-    var fivedayEl = document.getElementById ("five-day");
-    var currentWeatherEl = document.getElementById("current-weather");
+    const ViewEl = document.getElementById ("view-history");
 
-    
-    // Assigning API to variable //
-    const weatherApiKey =  "2b607860bec061627a4d53da7828381b";
+ 
+function getInfo() {
+    // takes what user inputs in search bar and puts in openweather url with API key to search for city
+    var city = searchInput.value;
+    // for current weather
+    var currentUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      weatherApiKey;
+      // for 5 day weather
+    var forecastUrl =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&appid=" +
+      weatherApiKey;
 
-    function weatherInfo(cityName) {
-        // Current weather get request from Open Weather API //
-        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + myapiKey;
-        axios.get(queryURL)
-    }
+
+      $(".removeRow").remove();
 }
